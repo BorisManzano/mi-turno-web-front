47d7180a3a211c04e55a1599f8f823d2578b62a2
foxxenflay <boristenduro09@gmail.com>
2023-11-13 19:39:53 -0300

    Avance del register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import Register from "./components/Register";
 function App() {
   return (
     <div className="App">
-      {/* <h1>HELLO WORLD!</h1> */}
-
-      {/* solo modifique esto ↓↓↓ */}
-      <Register />
+      <h1>HELLO WORLD!</h1>
     </div>
   );
 }
