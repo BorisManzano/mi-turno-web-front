7b777a68810f9904a833ff2e143ba87dd0abfd84
luis206r <luisr_02@outlook.es>
2023-11-13 17:13:37 -0500

    Change App restore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { ReservationHistory } from "./components/ReservationHistory";
-
 function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD!</h1>
-      <ReservationHistory />
     </div>
   );
 }
