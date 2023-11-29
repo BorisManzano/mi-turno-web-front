ed3a3ee35562ef55d1b9b8d1cb0714929bf4618b
rosmari ledesma <rochilesde@gmail.com>
2023-11-29 18:22:24 -0300

    agrego componente Route not found y su funcionalidad en la rutas no autorizas o existentes

--- src/navigation/OperatorRoutes.jsx
+++ src/navigation/OperatorRoutes.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import OperatorProfile from "../components/OperatorProfile";
 import { OperatorReservationsList } from "../components/OperatorReservationsList";
+import RouteNotFound from "../components/RouteNotFound";
 
 const OperatorRoutes = () => {
   return (
     <Routes>
       <Route path="/reservationsList" element={<OperatorReservationsList />} />
       <Route path="/profile" element={<OperatorProfile />} />
+      <Route
+          path="/*"
+          element={<RouteNotFound />}
+        />
     </Routes>
   );
 };
