1ca3478c2023def261df5214b0deff53405fe896
hernan789 <hernanlduarte@hotmail.com>
2023-11-30 11:36:58 -0300

    modifico dropdown, funcionalidad popup de error si ya no hay turnos disponibles para el día (dependiendo de la hora)

--- src/components/ReservationPanel/ReservationPanel.jsx
+++ src/components/ReservationPanel/ReservationPanel.jsx
@@ -9,6 +9,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Countdown from "../../commons/Countdown";
 import PopupReservation from "../../commons/popup-reservation";
+import PopupTimeOut from "../../commons/popup-timeOut";
 import { login } from "../../state/user";
 import "../ReservationPanel/ReservationPanel.scss";
 import {
@@ -21,16 +22,14 @@ import {
   StepLabel,
   Stepper,
 } from "@mui/material";
-
-import Popup from "../../commons/Popup";
 import { Today } from "@mui/icons-material";
-
 
 export default function ReservationPanel() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
   // variable para renderizas popup exitoso o de error
-  let state = true;
+  // let state = true;
+  const [state, setState] = React.useState(true);
   const [appointment, setAppointment] = React.useState({
     reservationId: "",
     branchId: "",
@@ -53,6 +52,7 @@ export default function ReservationPanel() {
   const [notAvailableSchedule, setNotAvilableSchedule] = React.useState("");
   const [reservedDay, setReservedDay] = React.useState([]);
   const [openingTime, setOpeningTime] = React.useState("");
+
   function handleNext() {
     setActiveStep((prev) => prev + 1);
   }
@@ -88,7 +88,7 @@ export default function ReservationPanel() {
       })
       .catch(() => console.log("NO BRANCHES AVAILABLE"));
   }, [reservationId]);
-
+  // const selectedDate = reservationId ? new Date(appointment.date) : null;
   //---------------------------------------------
   const [branchName, setBranchName] = React.useState("");
   //-------------------------------------------------------------
@@ -170,17 +170,36 @@ export default function ReservationPanel() {
 
     for (const schedule in schedulesCounter) {
       // console.log("schedulescONTUNER[schedule]", schedulesCounter[schedule]);
-      if (schedulesCounter[schedule] === capacity - 1) {
+      if (
+        schedulesCounter[schedule] === capacity - 1 &&
+        hourGetter() < schedule
+      ) {
         filteredSchedules.push(
           schedule + "   Último turno disponible en este horario!!"
         );
-      } else if (schedulesCounter[schedule] === capacity - 2) {
+      } else if (
+        schedulesCounter[schedule] === capacity - 2 &&
+        hourGetter() < schedule
+      ) {
         filteredSchedules.push(
           schedule + "   Últimos 2 turnos disponibles en este horario!!"
         );
-      } else if (schedulesCounter[schedule] < capacity - 1) {
+      } else if (
+        schedulesCounter[schedule] < capacity - 1 &&
+        hourGetter() < schedule
+      ) {
         filteredSchedules.push(schedule);
       }
+    }
+    if (filteredSchedules.length < 1) {
+      setState(false);
+      logicPopUp(".body", "add", "make-reservation-container-inactive");
+      logicPopUp(
+        ".fake-container-popup",
+        "remove",
+        "fake-container-popup-inactive"
+      );
+      logicPopUp(".fake-container-popup", "add", "fake-container-popup-active");
     }
     setSchedules(filteredSchedules.sort());
     handleNext();
@@ -449,9 +468,9 @@ export default function ReservationPanel() {
                   onChange={handleSelection}
                   disabled={enabled}
                 >
-                  <option value="" style={{ fontStyle: "italic" }}>
+                  <option value="" style={{ display: "none" }}>
                     {reservationId
-                      ? `${appointment.branchName} es tu sucursal elegida. Confirmala o elegí una nueva`
+                      ? `${appointment.branchName} es tu sucursal elegida. Confirmala o elegí una nueva:`
                       : "Elegí una sucursal:"}
                   </option>
                   {branches.map((branch) => (
@@ -494,8 +513,10 @@ export default function ReservationPanel() {
                       style={{ width: "100%", height: "35px" }}
                       onChange={handleScheduleSelection}
                     >
-                      <option value="">
-                        {reservationId ? appointment.schedule : data.schedule}
+                      <option value="" style={{ display: "none" }}>
+                        {reservationId
+                          ? appointment.schedule
+                          : "Elegí un horario"}
                       </option>
                       {schedules.map((schedule) => (
                         <option key={schedule} value={schedule}>
@@ -653,13 +674,11 @@ export default function ReservationPanel() {
                   }}
                   disablePast
                   onChange={handleDaySelector}
-                  shouldDisableDate={
-                    (day) =>
-                      reservedDay.some((date) =>
-                        dateComparator(day.$d, date)
-                      ) || day.$d.getDay() === 0 // Deshabilita el domingo
+                  shouldDisableDate={(day) =>
+                    reservedDay.some((date) => dateComparator(day.$d, date)) ||
+                    day.$d.getDay() === 0
                   }
-                  date={appointment.date ? new Date(appointment.date) : null}
+                  // defaultValue={selectedDate}
                 />
               </LocalizationProvider>
             ) : (
@@ -708,18 +727,13 @@ export default function ReservationPanel() {
           </Button>
         </Grid>
       </Box>
-      <Popup
-        title={`Se acabo el tiempo`}
-        text={`Puede presionar el boton continuar para refrescar la pagina y volver
-            a realizar la reserva`}
-        img={false}
-        redirect={true}
-      />
+      <PopupTimeOut />
       <PopupReservation
         state={state}
         reservationId={reservationIdParams || reservationId}
         editing={editing}
       />
+
       <ToastContainer />
     </div>
   );
