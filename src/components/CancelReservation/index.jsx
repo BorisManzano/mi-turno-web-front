8e447c3193b4d40b85005a8e942e9326dd520f3a
bautistagorchs <89758759+bautistagorchs@users.noreply.github.com>
2023-12-01 13:20:01 -0300

    Update index.jsx

--- src/components/CancelReservation/index.jsx
+++ src/components/CancelReservation/index.jsx
@@ -171,7 +171,7 @@ export const CancelReservation = () => {
             </div>
             <div className={s.horiz}>
               <p>Horario: </p>
-              <p className={s.Info}>{reservation.schedule.slice(0, 5)}</p>
+              <p className={s.Info}>{reservation.schedule.slice(0, 5)} hs</p>
             </div>
             <div className={s.horiz}>
               <p>Sucursal: </p>
